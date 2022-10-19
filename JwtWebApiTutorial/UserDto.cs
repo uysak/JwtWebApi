1c911d9e7fe0f075132d8dc31073dac151d6c763
Berkay Uysak <77589774+uysak@users.noreply.github.com>
2022-10-19 14:54:43 +0300

    Add project files.

---
+++ JwtWebApiTutorial/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace JwtWebApiTutorial
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+
+
+
+    }
+}
