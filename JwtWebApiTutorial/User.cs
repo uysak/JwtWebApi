337b27a5cb9977fd2751b8360c9b87102b941f81
Berkay Uysak <77589774+uysak@users.noreply.github.com>
2022-10-20 13:43:26 +0300

    refresh token added

--- JwtWebApiTutorial/User.cs
+++ JwtWebApiTutorial/User.cs
@@ -5,6 +5,8 @@
         public string Username { get; set; }
         public byte[] PasswordHash{ get; set; }
         public byte[] PasswordSalt { get; set; }
-
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; } = DateTime.Now;
+        public DateTime TokenExpires { get; set; }
     }
 }
